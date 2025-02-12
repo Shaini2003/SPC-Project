2cf1470c3b9fe8935afc5ee03f53dab983e68629
Shaini2003 <shinitharushika2020@gmail.com>
2025-02-12 15:03:56 +0530

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.Models.Entities;
+
+namespace Project.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        //{
+
+        //}
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Supplier> Suppliers { get; set; }
+    }
+}
---
+++ Models/Entities/Supplier.cs
@@ -0,0 +1,12 @@
+﻿namespace Project.Models.Entities
+{
+    public class Supplier
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public string? Phone { get; set; }
+
+
+    }
+}
